0ef7e2edf7d3faadabc3332dd8444b9a750cbb43
Marjan <mdmarjan.pg@gmail.com>
2026-07-14 23:38:54 +0600

    math topic add

--- Full Stack Development/Module 7 js/Hallo.js
+++ Full Stack Development/Module 7 js/Hallo.js
@@ -10,7 +10,19 @@ console.log(a1+' '+a2)
 /*
 Math topic in JS
 
-1.
+1. positive + , negative -
+2. freaction 5.26 12.26
+3. even, odd
+4. remainder 
+5. basic math operation
+6. percentage
+7. sum
+8. avg number
+9. max number / min number
+10. Series number
+11. calculation : area tringle
+12. Profit Share
+
 
 
 
