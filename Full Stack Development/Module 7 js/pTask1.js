61058fedc13b5f45a8ad519c2bcaa344837b643f
Marjan <mdmarjan.pg@gmail.com>
2026-07-15 16:31:39 +0600

    all js task complete

---
+++ Full Stack Development/Module 7 js/pTask1.js
@@ -0,0 +1,124 @@
+// task 1
+console.log("Hallo JavaScript!");
+
+let isName = "My name is Alex."
+console.log(isName);
+
+console.log("I am excited to become a Web Developer");
+
+// task 2
+let num1 = 25
+let num2 = 10
+console.log('Addition: ',num1+num2);
+console.log('Subtraction: ',num1-num2);
+console.log('Multiplication: ',num1*num2);
+console.log('Division:',num1/num2);
+console.log('Modulus:', num1%num2);
+
+// task 3
+let StudentName = 'Alex Hormuzi'
+let aGe = 20
+let country = "London"
+let isStudent = true;
+console.log('Student Name: '+StudentName);
+console.log('Age: '+aGe);
+console.log('Country: '+country);
+console.log('Is Student: '+isStudent);
+
+// task 4
+let a = 100
+let b = "Programming"
+let c = true
+let d = false
+let e = 3.1416
+console.log('a: '+typeof(a));
+console.log('b: '+typeof(b));
+console.log('c: '+typeof(c));
+console.log('d: '+typeof(d));
+console.log('e: '+typeof(e));
+
+// task 6
+let nAme = 'Alex'
+let age = 20
+let favoriteFood = 'Pizza'
+let favoriteLanguage = 'JavaScript'
+let lovesCoding = true
+console.log('Hello!');
+console.log('My name is '+nAme);
+console.log('I am '+age+' years old');
+console.log('My favorite food is '+favoriteFood);
+console.log('My favorite programming language is '+favoriteLanguage);
+console.log('I love coding: '+lovesCoding);
+
+// task 7
+let number1 = 50
+let number2 = 8
+console.log('Addition: ',number1+number2);
+console.log('Subtraction: ',number1-number2);
+console.log('Multiplication: ',number1*number2);
+console.log('Division:',number1/number2);
+console.log('Modulus:', number1%number2);
+
+// task 8
+let score = 50
+console.log('After += 10: ',score+=10);
+console.log('After -= 10: ',score-=5);
+console.log('After *= 10: ',score*=2);
+console.log('After /= 10: ',score/=5);
+console.log('After %= 10: ',score%=4);
+
+// task 9
+let price = 200
+let quantity = 3
+let discount = 10
+let totalPrice = price*quantity
+let Discount = totalPrice*10/100
+let finalPrice = totalPrice-Discount
+console.log('Total Price: ',totalPrice);
+console.log('Discount Amount: ',Discount);
+console.log('Final Price: ', finalPrice);
+
+// task 10
+// Question 1
+let x = 20
+let y = 5
+console.log(x+y); // 25
+
+// Question 2
+let Language = "JavaScript"
+console.log(typeof(Language)); // String 
+
+// Question 3
+let isOnline = false
+console.log(typeof(isOnline)); // boolean
+
+// Question 4
+let nUm = 10
+nUm += 5
+console.log(nUm); 
+
+// Question 5
+let b1 = 30
+let b2 = 7
+console.log(b1%b2); // 2
+
+// mini projects
+let fullName = "John Doe"
+let agE = 22
+let coUntry = 'Bangladesh'
+let FaVoriteLanguage = 'JavaScript'
+let dreamJob = 'Full-Stack Developer'
+let yearsGoal = 1
+let isLearningJS = true;
+
+console.log('========================\n      MY PROFILE\n========================\n\n'+
+   'Name: '+fullName+'\n'+
+   'Age: '+agE+'\n'+
+   'Country: '+coUntry+'\n'+
+   'Favorite Language: '+FaVoriteLanguage+'\n'+
+   'Dream Job: '+dreamJob+'\n'+
+   'Years to Reach Goal: '+yearsGoal+ '\n'+
+   'Learning JavaScript: '+isLearningJS+ '\n'+
+   '\n========================'
+
+)
