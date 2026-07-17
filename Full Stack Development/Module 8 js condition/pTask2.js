b89ed026f62847ce670866f5dd43c6de7d167fbb
Marjan <mdmarjan.pg@gmail.com>
2026-07-17 17:21:12 +0600

    ternery op include

--- Full Stack Development/Module 8 js condition/pTask2.js
+++ Full Stack Development/Module 8 js condition/pTask2.js
@@ -113,10 +113,117 @@ if(isHoliday||isWeekend){
 // -----------------------------------
 
 let uSername = 'admin'
-let passWord = '123456'
-if(uSername==='admin' && passWord==='123456'){
+let passWord = 123456
+if(uSername==='admin' && passWord===123456){
     console.log('Login successful');
 }
 else{
     console.log('Access Denide');
 }
+
+// -----------------------------------
+
+let mArk = 83
+if(mArk>=80 && mArk<=100){
+    console.log('A+');
+}else if(mArk>=70 && mArk<=79){
+    console.log('A');
+}else if(mArk>=60 && mArk<=69){
+    console.log('B');
+}else if(mArk>=50 && mArk<=59){
+    console.log('C');
+}else if(mArk>=40 && mArk<=49){
+    console.log('D');
+}else if(mArk>=33 && mArk<=39){
+    console.log('E');
+}else{
+    console.log('F');
+}
+
+// -----------------------------------
+
+/*
+BMI Calculator and Health Category
+Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a health category based on the BMI value. Use nested if-else statements to determine the health category.
+
+    - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+    - BMI < 18.5, you are underweight.
+    - BMI >= 18.5 and BMI <=24.9, you are normal.
+    - BMI >=25 and BMI <= 29.9, you are overweight.
+    - Otherwise, you are obese.
+*/
+
+let hEight = 1.6
+let weIght = 78
+
+let bmi = weIght/(hEight*hEight)
+if(bmi<18.5){
+    console.log('under weight');
+}else if(bmi >= 18.5 && bmi <= 24.9){
+    console.log('You are normal');
+}else if(bmi >= 25 && bmi <= 29.9){
+    console.log('You are overheight');
+}else{
+    console.log('you are obese');
+}
+
+// -----------------------------------
+
+let sEason = 3
+
+if(sEason === 4 || sEason === 5){
+    console.log('Summer');
+}else if(sEason === 6 || sEason === 7){
+    console.log('Monsson');
+}else if(sEason === 8 || sEason === 9){
+    console.log('Autumn');
+}else if(sEason === 10 || sEason === 11){
+    console.log('Late Autumn');
+}else if(sEason === 12 || sEason === 1){
+    console.log('Winter');
+}else if(sEason === 2 || sEason === 3){
+    console.log('Spring');
+}
+
+// -----------------------------------
+
+let Age = 19
+let hasID = true
+if(Age >= 18){
+    if(hasID){
+        console.log('eligible for vote');
+    }
+    else{
+        console.log('you eligible but you \n "havent ID"');
+    }
+}else{
+    console.log('not eligible \n "age low"');
+}
+
+// -----------------------------------
+
+let isLoggedin = true
+let isAdmin = false
+if(isLoggedin){
+    if(isAdmin){
+        console.log('Admin Dashboard');
+    }else{
+        console.log('User Dashboard');
+    }
+}else{
+    console.log('Please Login');
+}
+
+// -----------------------------------
+
+let nu = 4
+let re = nu %2 == 0 ? 'even' : 'odd'
+console.log(re);
+
+// -----------------------------------
+
+let agE = 15
+let gen = agE >= 18 ? 'adult' : 'minor'
+console.log(gen);
+
+// -----------------------------------
