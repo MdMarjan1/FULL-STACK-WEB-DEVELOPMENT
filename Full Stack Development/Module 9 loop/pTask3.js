9a2aaae0f5cc59acb34c5e957deb904a4f6b879a
Marjan <mdmarjan.pg@gmail.com>
2026-07-18 11:34:47 +0600

    while  loop  use

---
+++ Full Stack Development/Module 9 loop/pTask3.js
@@ -0,0 +1,38 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(i, 'Hello');
+}
+
+let a = 1;
+while (a <= 5) {
+  console.log(a, 'Hallo');
+  a++;
+}
+
+//-------------------------------------
+
+for(let s=1; s<=10; s++){
+  console.log(s);
+}
+
+//-------------------------------------
+
+let w = 10
+while(w>=1){
+  console.log(w);
+  w--
+}
+//-------------------------------------
+
+let rp = 1
+while(rp<=7){
+  console.log(rp,'Loop চলছে');
+  rp++
+}
+
+//-------------------------------------
+
+for(let even=1; even<=30; even++){
+  if(even%2==0){
+    console.log(even);
+  }
+}
