c8ffba0ca4e01b5c422de5b500fa9587ecced522
Marjan <mdmarjan.pg@gmail.com>
2026-07-18 22:28:52 +0600

    push pop

--- Full Stack Development/Module 10 Arrey/length.js
+++ Full Stack Development/Module 10 Arrey/length.js
@@ -1,3 +1,20 @@
 const number=[25, 30, 35, 45, 60, 10, 150]
 const count = number.length
 console.log(count);
+console.log(number);
+console.log(number[1]); //30
+
+
+let fruit = ['mango','jam','jackfruit','orange','rose','Dragon']
+console.log(fruit.length);
+console.log(fruit);
+console.log(fruit[0]); // mango
+console.log(fruit[3]); //orange
+let cox = fruit[fruit.length-1] // Dragon
+console.log(cox);
+fruit[1]='Vbanana'
+console.log(fruit);//[ 'mango', 'Vbanana', 'jackfruit', 'orange', 'rose', 'Dragon' ]
+
+
+
+
