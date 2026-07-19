566effdb6ff12797231be41c0d5161df39f68e4e
Marjan <mdmarjan.pg@gmail.com>
2026-07-19 15:53:39 +0600

    arrey for of loop

---
+++ Full Stack Development/Module 10 Arrey/arreyTraversal.js
@@ -0,0 +1,13 @@
+// arrey use for of loop 
+
+let numbers = [12, 33, 52, 60, 99, 100]
+for(const number of numbers){
+    console.log('current item',number);
+}
+
+// ---------------------------------------
+
+let numbe = [12, 33, 52, 60, 99, 100]
+for(let i = 0; i<numbe.length; i++){
+    console.log(i[numbe]);
+}
