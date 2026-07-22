fb9bf13671cc0d52a768325b0afc83ee0a954281
Marjan <mdmarjan.pg@gmail.com>
2026-07-22 15:18:03 +0600

    arrey practice task

--- Full Stack Development/Module 10 Arrey/pTask4.js
+++ Full Stack Development/Module 10 Arrey/pTask4.js
@@ -73,3 +73,61 @@ while(s<box.length){
     console.log(box[s]);
     s++
 }
+
+
+// ------------------------------------
+//13
+let x1 = [1, 2, 3, 4, 5]
+let x2 = [6, 7, 8, 9, 10]
+let x3 = x1.concat(x2)
+console.log(x3);
+
+// ------------------------------------
+//14
+let x4 = [11, 22, 33, 45, 66, 78, 89, 99]
+let x5 =x4.slice(1,4)
+console.log(x5);
+
+// ------------------------------------
+//15
+let x6 = ['jam', 'mango', 'orange', 'banana', 'malta']
+let x7 = x6.join(' ')
+console.log(x7);
+
+// ------------------------------------
+//16
+let x8 = ['am', 'jm', 'ko', 'gf', 'bf']
+let x9 = Array.isArray(x8)
+let x10 = Array.isArray('jm')
+console.log(x9);
+console.log(x10);
+
+// ------------------------------------
+//17
+let x11 = [11, 22, 33, 45, 66, 78, 89, 99]
+x11.reverse()
+console.log(x11);
+
+// ------------------------------------
+//18
+let x12 = ['jam', 'mango', 'orange', 'banana', 'malta']
+x12.sort()
+console.log(x12);
+
+// ------------------------------------
+//19
+let x13 = [11, 24, 10, 5, 55, 2, 64]
+for(let a=0; a<x13.length; a++){
+    if(x13[a] >10 ){
+        console.log(x13[a]);
+    }
+}
+
+// ------------------------------------
+//20
+let x14 = [10, 12, 13, 15, 17, 18]
+let sum = 0
+for(let b=0; b<x14.length; b++){
+    sum+=x14[b]
+}
+console.log(sum,'Arrey Sum'); //85
