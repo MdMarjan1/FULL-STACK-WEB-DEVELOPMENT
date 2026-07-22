8a4e1844e461ff91382108fd15bb7125a728048d
Marjan <mdmarjan.pg@gmail.com>
2026-07-22 11:34:47 +0600

    file name change

--- Full Stack Development/Module 10 Arrey/reverse.js
+++ Full Stack Development/Module 10 Arrey/reverse.js
@@ -1,3 +1,11 @@
 let number = [ 11, 12, 13, 14, 15, 16, 17]
 let rev = number.reverse()
-console.log(rev);
+// console.log(rev);
+
+// ---------------------------------------
+
+
+let numbe = [ 11, 12, 13, 14, 15, 16, 17]
+for(let jk=numbe.length-1; jk>=0; jk--){
+    console.log(numbe[jk]);
+}
