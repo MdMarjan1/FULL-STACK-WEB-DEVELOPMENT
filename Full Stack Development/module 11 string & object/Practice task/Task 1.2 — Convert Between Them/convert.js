3bb9a6d0a89db3c7977ac347bae98a3185e7dd5e
Marjan <mdmarjan.pg@gmail.com>
2026-07-23 22:43:27 +0600

    implement string to arrey, arrey to string conversion

---
+++ Full Stack Development/module 11 string & object/Practice task/Task 1.2 — Convert Between Them/convert.js
@@ -0,0 +1,9 @@
+// String to Convert Arrey
+let text = "JavaScript"
+let coNvert = text.split("")
+console.log(coNvert);
+
+// Arrey to String
+let letter = ["c","a","t"]
+let coNvert1 = letter.join("")
+console.log(coNvert1);
