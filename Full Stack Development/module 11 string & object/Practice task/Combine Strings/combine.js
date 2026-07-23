72216d25a7e7c63f03337f1a93a1972366d632f9
Marjan <mdmarjan.pg@gmail.com>
2026-07-23 23:44:49 +0600

    implement template literals examples

---
+++ Full Stack Development/module 11 string & object/Practice task/Combine Strings/combine.js
@@ -0,0 +1,12 @@
+let fristWord = "Hello"
+let lastWord = "World"
+let combind = fristWord.concat(" ",lastWord)
+console.log(combind);
+
+
+
+
+let word1 = "Hello"
+let word2 = "World"
+let mix = `${word1} ${word2}`
+console.log(mix);
