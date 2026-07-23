a80d8bbad5779e01ae62e16d7a047dc9e4b4e245
Marjan <mdmarjan.pg@gmail.com>
2026-07-23 23:13:56 +0600

    Practice JavaScript slice() method

---
+++ Full Stack Development/module 11 string & object/Practice task/Slicing Strings/slice.js
@@ -0,0 +1,9 @@
+let sentence = "Learning JavaScript is fun!";
+let wOrd = sentence.slice(0,8)
+console.log(wOrd); //Learning
+
+
+let sentence2 = "Learning JavaScript is fun!";
+let NegWord = sentence2.slice(-4)
+console.log(NegWord); //fun!
+
