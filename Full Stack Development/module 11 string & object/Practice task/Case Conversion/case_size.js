b258a3bcb0dbfb74bf0559812e6bb9e4c004c9af
Marjan <mdmarjan.pg@gmail.com>
2026-07-23 22:54:07 +0600

    complete toUppercase to Lowercase and trim

---
+++ Full Stack Development/module 11 string & object/Practice task/Case Conversion/case_size.js
@@ -0,0 +1,11 @@
+//  uppercase
+let uPcase = "JavaScript";
+console.log(uPcase.toUpperCase());
+
+// lowercase
+let lOwerCase = "JavaScript";
+console.log(lOwerCase.toLowerCase());
+
+// trim
+let whiteSpace = "  hi there       "
+console.log(whiteSpace.trim());
