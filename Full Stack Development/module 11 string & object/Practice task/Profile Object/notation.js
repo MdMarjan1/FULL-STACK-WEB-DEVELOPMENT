0586ea66d936050dcd1737bf3a51f00cb3520696
Marjan <mdmarjan.pg@gmail.com>
2026-07-24 20:33:55 +0600

    emplement dot & bracket notation

---
+++ Full Stack Development/module 11 string & object/Practice task/Profile Object/notation.js
@@ -0,0 +1,7 @@
+let car = { brand: "Toyota", model: "Corolla", year: 2022 };
+console.log(car.brand);
+console.log(car['model']);
+car['color']='blue'
+console.log(car);
+car.year=2023;
+console.log(car);
