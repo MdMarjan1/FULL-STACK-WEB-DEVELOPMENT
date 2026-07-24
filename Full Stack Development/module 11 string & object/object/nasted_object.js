8a316d1d32e8fd2412367c63671271463d1291cb
Marjan <mdmarjan.pg@gmail.com>
2026-07-24 17:12:21 +0600

    Practice nested objects and Object.entries()

--- Full Stack Development/module 11 string & object/object/nasted_object.js
+++ Full Stack Development/module 11 string & object/object/nasted_object.js
@@ -1,1 +1,24 @@
 // Nasted Object
+let Collage ={
+    class: 12,
+    subject: 'Science',
+    shift: 'Morning',
+    tacher: {
+        name: 'Selim khan',
+        age: 59,
+        skills:{
+            sub: 'Arts',
+            language: 'english',
+            color: ['black', 'Orange', 'Blue', 'Red']
+        }
+    }
+}
+console.log(Collage);
+console.log(Collage.subject);
+console.log(Collage['tacher']['name']);
+console.log(Collage.tacher['skills']['color']);
+console.log(Collage.tacher['skills']['color'][1]);
+
+
+// object.entries
+console.log(Object.entries(Collage));
