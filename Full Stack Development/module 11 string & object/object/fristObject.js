a23e99d4f54128fe58767e7395ff21dcff03b024
Marjan <mdmarjan.pg@gmail.com>
2026-07-24 11:38:30 +0600

    complete how to create Object

---
+++ Full Stack Development/module 11 string & object/object/fristObject.js
@@ -0,0 +1,12 @@
+let car = {
+    model: 'BMW',
+    reg: 2017,
+    color: 'Black',
+    carModel: ['black-c', 'Orange-c', 'Blue-c'],
+    isValid: true
+}
+console.log(car);
+console.log(car.model);
+console.log(car['reg']);
+console.log(car['carModel'][2]);
+console.log(car['carModel']);
