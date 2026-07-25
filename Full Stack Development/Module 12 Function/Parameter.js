62c6c66459bec635de46638e8eb24f2692177950
Marjan <mdmarjan.pg@gmail.com>
2026-07-26 00:52:47 +0600

    emplement parameter call and set

---
+++ Full Stack Development/Module 12 Function/Parameter.js
@@ -0,0 +1,29 @@
+function sayHello (name){
+    console.log("hallo Bro",name);
+}
+sayHello("Tamim")
+sayHello('nazim')
+sayHello('trina')
+
+
+function isPlus(a,b,c,d){
+    const total = a+b+c+d
+    console.log('plus number',total);
+}
+isPlus(10,5,10,5)
+
+function isMinus(a,b){
+    const sum = a-b
+    console.log('minus',sum);
+}
+isMinus(35,10)
+
+function isSqure(number){
+    const squre = number*number
+    console.log("SQ number",squre);
+}
+isSqure(5)
+
+isMinus(10,5)
+
+isSqure(7)
---
+++ Full Stack Development/Module 12 Function/function.js
@@ -0,0 +1,7 @@
+function sum(x,y){
+    const t = x+y;
+    return t
+}
+let total=sum(5,6)
+console.log(total);
+
