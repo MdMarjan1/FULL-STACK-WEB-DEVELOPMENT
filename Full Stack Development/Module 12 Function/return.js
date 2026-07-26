5c1e842d8c2a3bb5101aa1492412e4653b8e61e5
Marjan <mdmarjan.pg@gmail.com>
2026-07-26 14:36:00 +0600

    function return  set return to a variable

---
+++ Full Stack Development/Module 12 Function/return.js
@@ -0,0 +1,28 @@
+function isTotal(num1,num2){
+    const total = num1+num2
+    return total
+}
+const sum = isTotal(15,2)
+console.log(sum);
+
+const off = isTotal(20,20)
+console.log('your point',off);
+
+
+
+
+
+function cutHalf(number){
+    const sum = number/2
+    return sum
+}
+const all = cutHalf(50)
+console.log(all);
+
+
+//=========================================
+
+const isMer = isTotal(all,10)
+console.log("find mine",isMer); // 35
+
+
