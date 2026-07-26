b65e8d4b389ae25c7a991c3657f3e452ac26f245
Marjan <mdmarjan.pg@gmail.com>
2026-07-26 15:05:07 +0600

    function if else cheak  practice done

---
+++ Full Stack Development/Module 12 Function/isEven.js
@@ -0,0 +1,15 @@
+function isEven(num){
+    if(num%2===0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+const valueMan = isEven(20)
+console.log("this value",valueMan);
+
+// ======================================
+
+const gpg = isEven(25)
+console.log("your value is",gpg);
