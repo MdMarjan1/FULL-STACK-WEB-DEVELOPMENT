a90e166b11ecf8ae8f04bfa2dd1559bf8dd48006
Marjan <mdmarjan.pg@gmail.com>
2026-07-29 00:50:46 +0600

    add user value set & removefixed variable value

--- Full Stack Development/Module 13 problem solving/filter_condition.js
+++ Full Stack Development/Module 13 problem solving/filter_condition.js
@@ -1,14 +1,13 @@
 
 
-function isCondition(arr) {
-  let fixed = 50;
+function isCondition(arr, value) {
   let em = [];
   for (let x of arr) {
-    if (x > fixed) {
+    if (x > value) {
       em.push(x);
     }
   }
   return em;
 }
 
-console.log("Final output",isCondition([50, 60, 35, 122, 500, 6013, 40, 20, 79, 2005]));
+console.log("Final output",isCondition([50, 60, 35, 122, 500, 6013, 40, 20, 79, 2005],79));
