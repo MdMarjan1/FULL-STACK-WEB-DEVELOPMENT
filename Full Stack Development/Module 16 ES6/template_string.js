bec4af758915b4fcdb852a8b57c9155e54f949f7
Marjan <mdmarjan.pg@gmail.com>
2026-08-02 12:23:30 +0600

    ES6 practice

---
+++ Full Stack Development/Module 16 ES6/template_string.js
@@ -0,0 +1,17 @@
+let app1 =` my name is Usman
+ i read in class 6
+ i am German
+ i love USA`
+
+console.log(app1);
+
+// ================================
+
+
+let x = "Ovama"
+let y = "Trump"
+
+let z = `Frist name ${x} & Second name ${y}`
+console.log(z);
+
+// ================================
