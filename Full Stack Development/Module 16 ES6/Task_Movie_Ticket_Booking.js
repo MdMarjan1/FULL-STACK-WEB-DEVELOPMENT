59e4c7e77eed76b316f131de707a25adc13ef772
Marjan <mdmarjan.pg@gmail.com>
2026-08-03 12:42:25 +0600

    Movie ticket booking task use arrow function

---
+++ Full Stack Development/Module 16 ES6/Task_Movie_Ticket_Booking.js
@@ -0,0 +1,22 @@
+const bookTicket = (movie, seats=1, pricePerSeat=300) =>{
+    if(typeof movie !== "string"){
+        return `Invalid`
+    }
+    
+    if(seats < 1 || pricePerSeat < 1){
+        return `Invalid`
+    }
+
+    if(typeof movie === "string"){
+        let total = seats * pricePerSeat 
+        return `${movie}: ${seats} seat(s), Total ৳${total}`
+    }
+    else{
+        return `Invalid`
+    }
+}
+
+console.log(bookTicket("Dune"));
+console.log(bookTicket("Dune", 3));
+console.log(bookTicket("Dune", 2, 450));
+console.log(bookTicket(123,2));
