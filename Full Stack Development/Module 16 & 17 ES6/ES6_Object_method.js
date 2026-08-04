435f20f4126cfccaea4baab5caebfa33ef442793
Marjan <mdmarjan.pg@gmail.com>
2026-08-04 23:55:29 +0600

    es6 method practice

--- Full Stack Development/Module 16 & 17 ES6/ES6_Object_method.js
+++ Full Stack Development/Module 16 & 17 ES6/ES6_Object_method.js
@@ -8,18 +8,46 @@ let y1 = {
     class: 4
 }
 const entries = Object.entries(y1)
+console.log(entries); 
+
 
 const keys = Object.keys(y1)
-
-console.log(entries);
-
 console.log(keys);
+console.log(keys.length,"Object Keys length");
+ 
 
-for(let el of entries){
-    const [keyslll, value] = el
-    console.log(keyslll, value);
+const valu = Object.values(y1)
+console.log(valu);
+
+ // ================================
+
+ const obl ={
+    name: "samus",
+    id: 40,
+    role: 98,
+    sub: "Math"
+ }
+ const oblKeys = Object.keys(obl)
+ for(let ol of oblKeys){
+    console.log(ol);
+    console.log(ol, obl[ol]);
+ }
+
+// ================================
+// Arrey_Destructuring
+
+const match = {
+    name: "messi",
+    role: 45,
+    goal: 780
 }
-
+const enti = Object.entries(match)
+for(let uoo of enti){
+    const [key1, value1] = uoo
+    console.log(key1, value1);
+}
+ 
+// ================================
 
 // delete method
 let jk ={
@@ -38,18 +66,23 @@ let pk = {
     bankId: 696969,
     balance: 785
 }
-
 Object.seal(pk)
+// not delete
 delete pk.name
 console.log(pk);
+// no add
 pk.licence = 520
 console.log(pk);
+// only Edit
 pk.balance = 7500
 console.log(pk);
 
+// ================================
+
+
 
 // freeze
-//no value change
+//no value change no edit no add
 let opk = {
     name: "SL abdul",
     bankId: 66564,
