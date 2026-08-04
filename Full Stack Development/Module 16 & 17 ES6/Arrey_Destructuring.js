d9a109e03f53bc2b1d48a6118f3bef86f0759957
Marjan <mdmarjan.pg@gmail.com>
2026-08-04 22:38:18 +0600

    arrey Destructuring

--- Full Stack Development/Module 16 & 17 ES6/Arrey_Destructuring.js
+++ Full Stack Development/Module 16 & 17 ES6/Arrey_Destructuring.js
@@ -5,26 +5,10 @@ console.log(ten, twenty, thirthy, forthy, eighty); //10 20 30 40 80
 
 // ================================
 
-// Destructuring for Object
-let a1 = {
-    name: "marjna",
-    role: 56,
-    group: "Science",
-    year: 2027
-}
-const {role, year} = a1
-console.log(role, year);
+const a11 = [10, 20, 30, 40, 50, 60, 70, 80]
+const [frist, second, , , ,sixty] = a11
+console.log(frist, second, sixty );
 
-// ================================
 
-let a2 ={
-    price: 56,
-    model: 'Orange',
-    brand: 'cho--cho',
-    exam: {
-        nam: 'ssc',
-        year: 2026
-    }
-}
-const {price, brand, model:newModel, exam:{nam}} = a2
-console.log(price, brand, newModel, nam);
+
+
