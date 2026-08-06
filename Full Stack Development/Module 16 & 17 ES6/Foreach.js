51d2280d47a4f457ec06405e64e32ccbfa782c0b
Marjan <mdmarjan.pg@gmail.com>
2026-08-06 13:42:20 +0600

    filter push

---
+++ Full Stack Development/Module 16 & 17 ES6/Foreach.js
@@ -0,0 +1,4 @@
+let x = [ 10, 20, 30, 40, 50, 60]
+x.forEach((elm, inx)=>{
+    console.log(elm, inx);
+})
---
+++ Full Stack Development/Module 16 & 17 ES6/filter.js
@@ -0,0 +1,5 @@
+let valu = [10, 20, 30, 40, 500, 1000]
+const chek = valu.filter((high)=>{
+   return high > 30
+})
+console.log(chek);
