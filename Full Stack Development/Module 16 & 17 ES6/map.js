11e5e17c613720e3d45fe050edcc1c9058e045f4
Marjan <mdmarjan.pg@gmail.com>
2026-08-06 13:14:03 +0600

    map method js

---
+++ Full Stack Development/Module 16 & 17 ES6/map.js
@@ -0,0 +1,13 @@
+let numbers = [10, 20, 30, 40, 50, 60]
+const doubel = numbers.map((elm)=>{
+    return elm*2
+})
+console.log(doubel);
+
+
+let numd = [2, 6, 8, 10]
+const deb = numd.map((app)=>{
+    return app +2
+})
+console.log(deb);
+console.log(numd);
