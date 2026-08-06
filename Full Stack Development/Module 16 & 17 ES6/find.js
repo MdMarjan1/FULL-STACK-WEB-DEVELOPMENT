d29762ec0545f71e3328233b00ccab570d85ff16
Marjan <mdmarjan.pg@gmail.com>
2026-08-06 17:07:55 +0600

    Es6 map built in function done

---
+++ Full Stack Development/Module 16 & 17 ES6/find.js
@@ -0,0 +1,7 @@
+let valu = [10, 20, 30, 40, 500, 1000]
+
+const find = valu.find((top)=>{
+    return top > 40
+})
+console.log(find);
+// find out frist element based on condition
--- Full Stack Development/Module 16 & 17 ES6/map.js
+++ Full Stack Development/Module 16 & 17 ES6/map.js
@@ -11,3 +11,18 @@ const deb = numd.map((app)=>{
 })
 console.log(deb);
 console.log(numd);
+
+// ================================
+
+let arr1 = [10, 20, 30, 40, 50, 60, 70]
+const double1 = arr1.map((elm1)=>{
+    return elm1*2
+})
+console.log(doubel);
+
+
+// ================================
+
+let abc = [69, 59, 89, 96]
+const double2 = abc.map((acc)=> acc*2)
+console.log(double2);
