5d2eeb721a93660accb499bf860aad25a05b2040
Marjan <mdmarjan.pg@gmail.com>
2026-08-06 17:35:56 +0600

    forEach complete

--- Full Stack Development/Module 16 & 17 ES6/Foreach.js
+++ Full Stack Development/Module 16 & 17 ES6/Foreach.js
@@ -2,3 +2,10 @@ let x = [ 10, 20, 30, 40, 50, 60]
 x.forEach((elm, inx)=>{
     console.log(elm, inx);
 })
+
+// ================================
+
+let se = [10, 22, 33, 44, 55, 66, 77, 88]
+se.forEach((jk, odx )=>{
+    console.log(jk, odx);
+})
