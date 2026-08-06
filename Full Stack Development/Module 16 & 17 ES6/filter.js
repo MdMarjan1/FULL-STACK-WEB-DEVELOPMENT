47a3b302fd6fddec50adf32bab70d2abed0c7750
Marjan <mdmarjan.pg@gmail.com>
2026-08-06 17:42:12 +0600

    filter function method done

--- Full Stack Development/Module 16 & 17 ES6/filter.js
+++ Full Stack Development/Module 16 & 17 ES6/filter.js
@@ -3,3 +3,11 @@ const chek = valu.filter((high)=>{
    return high > 30
 })
 console.log(chek);
+
+// ================================
+
+let gol = [45, 78, 90, 45, 500]
+const ql = gol.filter((ros)=>{
+   return ros >=90
+})
+console.log(ql);
