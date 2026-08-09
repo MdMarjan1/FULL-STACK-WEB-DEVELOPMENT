f31550f0f18d048e2a3152190705c8fb250e19f4
Marjan <mdmarjan.pg@gmail.com>
2026-08-09 19:19:34 +0600

    practice find user ID with find method

--- Full Stack Development/Module 19 Problem Solving/dataCheak_find_method.js
+++ Full Stack Development/Module 19 Problem Solving/dataCheak_find_method.js
@@ -1,16 +1,17 @@
-const findUserId = (users, id) =>{
-    const cheaKid = users.find((user)=>{
-      return  user.id === id
-    })
-    return cheaKid
-}
+const findUserId = (users, id) => {
+  const cheaKid = users.find((user) => {
+    return user.id === id;
+  });
 
+  if (!cheaKid) return `NOt Found`;
+};
 
 let users = [
-    {id:14, name: "rafi"},
-    {id:456, name: "Proma"},
-    {id:45, name: "shawon"}
-]
+  { id: 14, name: 'rafi' },
+  { id: 456, name: 'Proma' },
+  { id: 45, name: 'shawon' },
+];
 
-console.log(findUserId(users,14));
-console.log(findUserId(users,456));
+console.log(findUserId(users, 14));
+console.log(findUserId(users, 456));
+console.log(findUserId(users, 500));
