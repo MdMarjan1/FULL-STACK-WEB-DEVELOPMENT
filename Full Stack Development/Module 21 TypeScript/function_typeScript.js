749185fff8804e87d1e8d0d95616cf256213043a
Marjan <mdmarjan.pg@gmail.com>
2026-08-17 19:49:26 +0600

    function typeScript set

---
+++ Full Stack Development/Module 21 TypeScript/function_typeScript.js
@@ -0,0 +1,11 @@
+"use strict";
+function calculate(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+console.log(calculate(5, 5));
+function inAll(name, role, pres) {
+    return false;
+}
+console.log(inAll('marjan', 45));
+console.log(calculate(50, 45));
---
+++ Full Stack Development/Module 21 TypeScript/function_typeScript.ts
@@ -0,0 +1,14 @@
+function calculate(num1:number, num2:number):number{
+    const sum = num1+num2;
+    return sum
+}
+console.log(calculate(5,5))
+
+
+function inAll(name:string, role:number, pres?:boolean):boolean{
+    return false
+}
+console.log(inAll('marjan', 45));
+
+console.log(calculate(50,45));
+
