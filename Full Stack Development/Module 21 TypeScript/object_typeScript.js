a07985ccc3e27e05b3afd2497357dc0351bbf11d
Marjan <mdmarjan.pg@gmail.com>
2026-08-17 16:13:29 +0600

    object property fixed

---
+++ Full Stack Development/Module 21 TypeScript/object_typeScript.js
@@ -0,0 +1,13 @@
+"use strict";
+let isObj1 = {
+    name: 'mark',
+    roll: 45,
+    present: true
+};
+console.log(isObj1.name);
+let isObj2 = {
+    name: 'ak',
+    roll: 456,
+    present: false
+};
+console.log(isObj2);
---
+++ Full Stack Development/Module 21 TypeScript/object_typeScript.ts
@@ -0,0 +1,17 @@
+let isObj1:{name:string, roll:number, present:boolean} ={
+    name: 'mark',
+    roll: 45,
+    present: true
+}
+console.log(isObj1.name)
+
+
+// optional property
+let isObj2:{name:string, roll:number, present:boolean, id?:number} ={
+    name: 'ak',
+    roll: 456,
+    present: false
+}
+
+console.log(isObj2);
+
