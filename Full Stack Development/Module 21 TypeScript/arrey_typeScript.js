9245b2445e69c8315b31d8e4096ce273ad9d3be7
Marjan <mdmarjan.pg@gmail.com>
2026-08-17 15:46:06 +0600

    arrey typeScript done

---
+++ Full Stack Development/Module 21 TypeScript/arrey_typeScript.js
@@ -0,0 +1,8 @@
+"use strict";
+let isArr1 = ['car', 'Bus', 'Bike', 'Jips', 'Fruits'];
+console.log(isArr1);
+let isArr2 = ['mango', 'orange', 'banana'];
+isArr2.push('apple');
+console.log(isArr2);
+let isArr3 = [69, 89, 99, 199, 299];
+console.log(isArr3);
