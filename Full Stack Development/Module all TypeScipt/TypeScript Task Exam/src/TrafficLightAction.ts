90f1d0f27afa1b5f925775f0d0823fc87b59069e
Marjan <mdmarjan.pg@gmail.com>
2026-08-23 02:19:12 +0600

    traffic light action

---
+++ Full Stack Development/Module all TypeScipt/TypeScript Task Exam/src/TrafficLightAction.ts
@@ -0,0 +1,19 @@
+type valu = "red" | "yellow" | "green"
+const getTrafficAction = (light: valu):string =>{
+    if(light === "red"){
+        return `Stop`;
+    }
+    else if(light === "yellow"){
+        return `Slow Down`;
+    }
+    else if(light === "green"){
+        return `Go`
+    }
+
+    return `Invalid`
+}
+
+console.log(getTrafficAction("red"));
+console.log(getTrafficAction("yellow"));
+console.log(getTrafficAction("green"));
+
