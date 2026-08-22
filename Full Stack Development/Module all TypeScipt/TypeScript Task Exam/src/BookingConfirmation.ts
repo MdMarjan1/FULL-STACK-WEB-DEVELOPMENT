1747f6d33affe91062c27ec509a49edb002e9b67
Marjan <mdmarjan.pg@gmail.com>
2026-08-23 01:18:14 +0600

    Table Booking Confermation ts Task complete

--- Full Stack Development/Module all TypeScipt/TypeScript Task Exam/src/BookingConfirmation.ts
+++ Full Stack Development/Module all TypeScipt/TypeScript Task Exam/src/BookingConfirmation.ts
@@ -1,13 +1,10 @@
 interface booking {
-    name: string;
-    guests: number;
-    time: string;
+  name: string;
+  guests: number;
+  time: string;
 }
-const formatBookingConfirmation = (booking: booking): string =>{
-    // return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`
-    return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`
-
-}
-console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }));
-console.log(formatBookingConfirmation({ name: "Rahim", guests: 2, time: "8:30 PM" }));
-
+const formatBookingConfirmation = (booking: booking): string => {
+  return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`;
+};
+console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }),);
+console.log(formatBookingConfirmation({ name: "Rahim", guests: 2, time: "8:30 PM" }),);
